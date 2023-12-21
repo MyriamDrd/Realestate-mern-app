e366d6319364af4cc95f068ea50d1d75cb46d12b
Myriam <dridimyriam94@gmail.com>
2023-12-21 21:58:12 +0100

    Header component integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"; // on peut dire que c'est comme nav ul li..
 import Home from "./pages/Home.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
 import About from "./pages/About.jsx";
+import Header from "./components/Header.jsx";
 
 export default function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
+    
+    <BrowserRouter>
+       <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-
-
-
