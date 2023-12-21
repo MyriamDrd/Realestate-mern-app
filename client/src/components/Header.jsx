d39d0b188accd2ed406143b85aedcc760963482b
Myriam <dridimyriam94@gmail.com>
2023-12-21 21:58:35 +0100

    Header component integration

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="bg-slate-200 shadow-md">
+            <span className="text-state-500">MWM</span>
+            <span className="text-state-700">Estate</span>
+          </h1>
+        </Link>
+        <from className=" bg-slate-100 p-3 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent"
+          />
+          <FaSearch className="text-slate-600" />
+        </from>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>{" "}
+          </Link>
+
+          <Link to="/About">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="sign-in">
+            <li className=" text-slate-700 hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
