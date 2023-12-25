e12ee525333fd12e0dee38344c130b58d1de2efa
Myriam <dridimyriam94@gmail.com>
2023-12-25 21:31:06 +0100

    integrate routes  & express.json

--- api/index.js
+++ api/index.js
@@ -1,18 +1,27 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err)=> {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.log(`Error connecting to MongoDB: ${err}`);
-})
+  });
 
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log('server is runningon port 3000');
-}
-) 
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("server is runningon port 3000");
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
