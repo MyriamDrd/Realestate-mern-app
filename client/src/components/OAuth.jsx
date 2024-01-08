40184ef945b856b2ae7f7ae5797aee011a398c52
Myriam <dridimyriam94@gmail.com>
2024-01-08 19:55:31 +0100

    minor change

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,8 +2,7 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
-import {useNavigate} from "react-router-dom"
-
+import { useNavigate } from "react-router-dom";
 const OAuth = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -12,12 +11,9 @@ const OAuth = () => {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-
       const res = await fetch("/api/auth/google", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           name: result.user.displayName,
           email: result.user.email,
@@ -27,7 +23,6 @@ const OAuth = () => {
       const data = await res.json();
       dispatch(signInSuccess(data));
       navigate("/");
-      
     } catch (error) {
       console.log("could not sign in with google");
     }
@@ -36,15 +31,9 @@ const OAuth = () => {
     <button
       onClick={handleGoogleClick}
       type="button"
-      className="
-         bg-red-700
-         text-white
-         p-3
-         rounded-lg 
-         uppercase
-         hover:opacity-95
-         "
+      className=" bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 "
     >
+      {" "}
       Continue with google
     </button>
   );
