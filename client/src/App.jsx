79672eca46981cb159f4845e005a8551e5259919
Myriam <dridimyriam94@gmail.com>
2024-01-08 19:55:06 +0100

    Update header with private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
 import About from "./pages/About.jsx";
 import Header from "./components/Header.jsx";
+import PrivateRoute from "./components/Privateroute.jsx";
 
 export default function App() {
   return (
@@ -17,7 +18,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />} >
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
