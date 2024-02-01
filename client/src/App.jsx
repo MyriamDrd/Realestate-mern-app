7e7e3421d9140613cf1cd39df5ed126b4b778e6a
Myriam <dridimyriam94@gmail.com>
2024-02-01 21:06:22 +0100

    listing page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,32 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // on peut dire que c'est comme nav ul li..
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import Profile from "./pages/Profile.jsx";
 import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
 import Header from "./components/Header.jsx";
-import PrivateRoute from "./components/Privateroute.jsx";
-
-import CreateListing from './pages/CreateListing.jsx';
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import CreateListing from "./pages/CreateListing.jsx";
+import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
 
 export default function App() {
   return (
-    
     <BrowserRouter>
-       <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route element={<PrivateRoute />} >
-        <Route path="/profile" element={<Profile/>} />
-
+        <Route path="/listing/:listingId" element={<Listing />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
-
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
