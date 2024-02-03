e4a9315dd5e72d24e8d8e60203b73b2bec316fbd
Myriam <dridimyriam94@gmail.com>
2024-02-03 19:24:05 +0100

    share icon & copy link to clipboard function

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,62 +1,81 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper";
-import { Navigation } from "swiper/modules";
-import "swiper/css/bundle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+import { FaShare } from "react-icons/fa";
+
 
 const Listing = () => {
   SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   const params = useParams();
 
   useEffect(() => {
     const fetchListing = async () => {
       try {
-        setLoading(true);
+        setLoading(true)
         const res = await fetch(`/api/listing/get/${params.listingId}`);
         const data = await res.json();
         if (data.success === false) {
           setError(true);
-          setLoading(false);
+          setLoading(false)
           return;
         }
         setListing(data);
-      } catch (error) {
-        setError(true);
         setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true)
+        setLoading(false)
       }
-    };
+    }
     fetchListing();
-  }, [params.listingId]);
-
+  }, [params.listingId])
   return (
     <main>
       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
-      {error && (
-        <p className="text-center my-7 text-2xl">Something went wrong</p>
-      )}
+      {error && <p className="text-center my-7 text-2xl">Something went wrong</p>}
       {listing && !loading && !error && (
         <div>
           <Swiper navigation>
-            {listing.imageUrls.map((url) => {
+            {listing.imageUrls.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className="h-[550px]"
+                  className='h-[550px]'
                   style={{
                     background: `url(${url}) center no-repeat`,
-                    backgroundSize: "cover",
+                    backgroundSize: 'cover',
                   }}
                 ></div>
-              </SwiperSlide>;
-            })}
+              </SwiperSlide>
+            ))}
           </Swiper>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
         </div>
       )}
     </main>
   );
-};
-
+}
+ 
 export default Listing;
