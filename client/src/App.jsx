0b8cee2ac7421fdd1aed5023945414d23b7491c6
Myriam <dridimyriam94@gmail.com>
2024-02-03 19:25:42 +0100

    listing page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import SignIn from "./pages/SignIn.jsx";
-import SignUp from "./pages/SignUp.jsx";
-import About from "./pages/About.jsx";
-import Profile from "./pages/Profile.jsx";
-import Header from "./components/Header.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
-import CreateListing from "./pages/CreateListing.jsx";
-import UpdateListing from "./pages/UpdateListing.jsx";
-import Listing from "./pages/Listing.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
+import Header from './components/Header.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx';
+import CreateListing from './pages/CreateListing.jsx';
+import UpdateListing from './pages/UpdateListing.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/listing/:listingId" element={<Listing />} />
-        <Route element={<PrivateRoute />}>
+        <Route element={<PrivateRoute />} >
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          <Route
-            path="/update-listing/:listingId"
-            element={<UpdateListing />}
-          />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
